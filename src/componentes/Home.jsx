f923fcc8450c1ca8a413318f0ffe3b9de65acd81
camilafbarcellos <79776234+camilafbarcellos@users.noreply.github.com>
2024-07-02 11:19:51 -0300

    feat: add dynamic home page

--- src/componentes/Home.jsx
+++ src/componentes/Home.jsx
@@ -1,19 +1,29 @@
-const Home = () => (
-    <div className="cover-container d-flex h-100 p-3 mx-auto flex-column text-center">
-        <main role="main" className="inner cover">
-            <h1 className="cover-heading">Bem-vindo ao e-Lib!</h1>
-            <p className="lead">e-Lib é uma plataforma online para gerenciar e explorar uma vasta coleção de livros. Faça o login para começar a explorar nossa biblioteca ou cadastrar novos livros.</p>
-            <p className="lead">
-                <a href="/login" className="btn btn-lg btn-secondary">Login</a>
-            </p>
-        </main>
+import { getUsuario } from '../seguranca/Autenticacao';
 
-        <footer className="mastfoot mt-auto">
-            <div className="inner">
-                <p>&copy; Camila Barcellos</p>
-            </div>
-        </footer>
-    </div>
-);
+const Home = () => {
+
+    const usuario = getUsuario();
+
+    return (
+        <div className="cover-container d-flex h-100 p-3 mx-auto flex-column text-center">
+            <main role="main" className="inner cover">
+                <h1 className="cover-heading">Bem-vindo ao e-Lib!</h1>
+                <p className="lead">e-Lib é uma plataforma online para gerenciar e explorar a sua própria coleção de livros. 
+                Faça o login para começar ou acesse já a sua biblioteca!</p>
+                <p className="lead">
+                    <a href={usuario ? "/privado/livros" : "/login"} className="btn btn-lg btn-secondary">
+                        {usuario ? "Visitar biblioteca" : "Login"}
+                    </a>
+                </p>
+            </main>
+
+            <footer className="mastfoot mt-auto">
+                <div className="inner">
+                    <p>&copy; Camila Barcellos, 2024</p>
+                </div>
+            </footer>
+        </div>
+    );
+};
 
 export default Home;
